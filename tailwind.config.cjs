abd5ee9122f3f67a84c71f0991ce4e43e364e05a
shocker <fmju96@gmail.com>
2023-09-16 20:47:55 -0300

    hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'tall': { 'raw': '(min-height: 1200px)' },
+			},
+		},
 	},
 	plugins: [],
 }
